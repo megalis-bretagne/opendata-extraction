6b26c2fbf60c6518ecf64614e4f3250948550c64
guenneuguesy <yann.guenneugues@sib.fr>
2022-06-24 12:56:03 +0200

    test full text

--- app/tasks/publication_tasks.py
+++ app/tasks/publication_tasks.py
@@ -386,19 +386,19 @@ def init_document(data, acte, parametrage, publication, urlPDF, typology):
     # data["literal.siren"] = "242900710"
 
     data["commit"] = 'true'
-    # data["literal.hash"] = acte.hash
-    # data["literal.publication_id"] = publication.id
-    # data["literal.filepath"] = urlPDF
-    # # data["literal.stream_content_type"] = data['metadata']["Content-Type"]
-    # # etat publication
-    # data["literal.est_publie"] = False
-    # data["literal.opendata_active"] = parametrage.open_data_active
-    # data["literal.date_budget"] = publication.date_budget
-    #
-    # # partie métadata (issu du fichier metadata.json de pastell)
-    # data["literal.date"] = publication.date_de_lacte.strftime("%Y-%m-%dT%H:%M:%SZ")
-    # now = datetime.now()  # current date and time
-    # data["literal.date_de_publication"] = now.strftime("%Y-%m-%dT%H:%M:%SZ")
+    data["literal.hash"] = acte.hash
+    data["literal.publication_id"] = publication.id
+    data["literal.filepath"] = urlPDF
+    # data["literal.stream_content_type"] = data['metadata']["Content-Type"]
+    # etat publication
+    data["literal.est_publie"] = False
+    data["literal.opendata_active"] = parametrage.open_data_active
+    data["literal.date_budget"] = publication.date_budget
+
+    # partie métadata (issu du fichier metadata.json de pastell)
+    data["literal.date"] = publication.date_de_lacte.strftime("%Y-%m-%dT%H:%M:%SZ")
+    now = datetime.now()  # current date and time
+    data["literal.date_de_publication"] = now.strftime("%Y-%m-%dT%H:%M:%SZ")
     #
     # data["literal.description"] = publication.objet
     # data["literal.documentidentifier"] = publication.numero_de_lacte
